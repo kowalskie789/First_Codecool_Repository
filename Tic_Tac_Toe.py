f3c1cbad7aab0afed46c32a54a03efcaa290dc89
kowalskie789@gma <kowalskie789@gmail.com>
2022-10-31 14:55:21 +0100

    TickTacToe update

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,16 +1,22 @@
+from msilib.schema import tables
+from operator import truediv
+
+
 def get_board():
     return [[ '.','.','.' ],[ '.','.','.' ],[ '.','.','.' ]]
                   #A              #B              #C
 #print(get_board()[2][0])
                   #C#1 
 
+tablica = get_board() #ważne!!! 
+
 def print_board():
     print("   1   2   3")
-    print("A ",get_board()[0][0],"|",get_board()[0][1],"|",get_board()[0][2],"")
+    print("A ",tablica[0][0],"|",tablica[0][1],"|",tablica[0][2],"")
     print("  ---+---+---")
-    print("A ",get_board()[1][0],"|",get_board()[1][1],"|",get_board()[1][2],"")
+    print("B ",tablica[1][0],"|",tablica[1][1],"|",tablica[1][2],"")
     print("  ---+---+---")
-    print("A ",get_board()[2][0],"|",get_board()[2][1],"|",get_board()[2][2],"")
+    print("C ",tablica[2][0],"|",tablica[2][1],"|",tablica[2][2],"")
 
 def get_human_coordinates():
 
@@ -19,25 +25,50 @@ def get_human_coordinates():
         move = input("Incorecct. Whats your next move?")
     if move[0] == 'A' or move[0] == 'a': #to głupie może jakoś zmienić?  
         if move[1] == '1':
-            return "[0][0]"
+            return [0,0]
         elif move[1] == '2':
-            return "[0][1]"
+            return [0,1]
         else:
-            return "[0][3]"
+            return [0,2]
     elif move[0] == 'B' or move[0] == 'b': #to głupie może jakoś zmienić?  
         if move[1] == '1':
-            return "[1][0]"
+            return [1,0]
         elif move[1] == '2':
-            return "[1][1]"
+            return [1,1]
         else:
-            return "[1][3]"
+            return [1,2]
     if move[0] == 'C' or move[0] == 'c': #to głupie może jakoś zmienić?  
         if move[1] == '1':
-            return "[2][0]"
+            return [2,0]
         elif move[1] == '2':
-            return "[2][1]"
+            return [2,1]
         else:
-            return "[2][3]"
+            return [2,2]
 
 
-print(get_human_coordinates())
+def implement_move(coordinate,znak):
+    tablica[coordinate[0]][coordinate[1]] = znak
+    return
+
+def onevone():
+    X=True
+    while True:
+        print_board()
+        while X:
+            holder = get_human_coordinates()
+            if tablica[holder[0]][holder[1]] not in "X,O".split(","):
+                implement_move(holder,"X")
+                X= False
+                break
+            else:
+                print("Wrong move, try again")
+        else:
+            holder = get_human_coordinates()
+            if tablica[holder[0]][holder[1]] not in "X,O".split(","):
+                implement_move(holder,"O")
+                X= True
+            else:
+                print("Wrong move, try again")
+            
+
+onevone()
