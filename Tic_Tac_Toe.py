3a1cab1e52903e5e26e9a75e0783fb169d29084c
kowalskie789@gma <kowalskie789@gmail.com>
2022-11-02 18:53:32 +0100

    update

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -21,9 +21,14 @@ def print_board():
 
 def get_human_coordinates():
 
-    move = input("Whats your next move?").upper()
+    move = input("Whats your next move? ").upper()
+    if move == "QUIT":
+        quit()
     while move not in 'A1,A2,A3,B1,B2,B3,C1,C2,C3'.split(","): #dodać drugi warunek:P
-        move = input("Incorecct. Whats your next move?").upper()
+        print_board()
+        move = input("Incorecct. Whats your next move? ").upper()
+        if move == "QUIT":
+            quit()
     if move[0] == 'A':  
         if move[1] == '1':
             return [0,0]
@@ -55,9 +60,8 @@ def onevone():
     X=True
     while True:
         print_board()
-        get_winning_player()
-        if is_board_full(tablica): #to na chwilkę tutaj będzie 
-            quit()
+        if is_board_full() or get_winning_player(): 
+            quit() # zamienić na main menu później? 
         while X:
             holder = get_human_coordinates()
             if tablica[holder[0]][holder[1]] not in "X,O".split(","):
@@ -66,6 +70,8 @@ def onevone():
                 break
             else:
                 print("Wrong move, try again")
+                print_board()
+                
         else:
             holder = get_human_coordinates()
             if tablica[holder[0]][holder[1]] not in "X,O".split(","):
@@ -73,27 +79,28 @@ def onevone():
                 X= True
             else:
                 print("Wrong move, try again")
+                
 
 def get_winning_player():
-    if tablica[0][0]==tablica[0][1]==tablica[0][2] and tablica[0][0]!='.':
-        return print("Wygrał ",tablica[0][0]) # to może zmienić na kejsy to może będzie ładniej i dodać quit() albo main menu
-    if tablica[1][0]==tablica[1][1]==tablica[0][2] and tablica[1][0]!='.':
-        return print("Wygrał ",tablica[1][0])
-    if tablica[2][0]==tablica[2][1]==tablica[2][2] and tablica[2][0]!='.':
-        return print("Wygrał ",tablica[2][0])
-    if tablica[0][0]==tablica[1][0]==tablica[2][0] and tablica[0][0]!='.':
-        return print("Wygrał ",tablica[0][0])
-    if tablica[0][1]==tablica[1][1]==tablica[2][1] and tablica[0][1]!='.':
-        return print("Wygrał ",tablica[0][1])
-    if tablica[0][2]==tablica[1][2]==tablica[2][2] and tablica[0][2]!='.':
-        return print("Wygrał ",tablica[0][2])
+    
+    # Checking for rows for win
+    for row in range(0, 3):
+        if tablica[row][0] == tablica[row][1] == tablica[row][2] and tablica[row][0] != '.':
+            return print("Wygrał ", tablica[row][0])
+    
+    # Checking for columns for win
+    for col in range(0, 3):
+        if tablica[0][col] == tablica[1][col] == tablica[2][col] and tablica[0][col] != '.':
+            return print("Wygrał ", tablica[0][col])
+        
+    # Checks for diagonals for win
     if tablica[0][0]==tablica[1][1]==tablica[2][2] and tablica[0][0]!='.':
         return print("Wygrał ",tablica[0][0])
     if tablica[0][2]==tablica[1][1]==tablica[2][0] and tablica[0][2]!='.':
         return print("Wygrał ",tablica[0][2])
     
-def is_board_full(table):
-    if not any("." in x for x in table): #nie wiem jak to działa 
+def is_board_full():
+    if not any("." in x for x in tablica): #nie wiem jak to działa 
         print("tablica pełna koniec gry")
         return True
     return False
