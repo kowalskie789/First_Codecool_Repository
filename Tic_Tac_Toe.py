e3afcec50a92bf0647948b08bb63254df04204a5
Hubert Lewandowski <115325324+HLGitHuber@users.noreply.github.com>
2022-11-06 00:37:48 +0100

    naprawione bugi zwiazane z unbeatable ai
    na łopatologie i bardzo duzo if-ow, ale dziala i chyba zabezpieczylem pod kazdym katem 😄

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -140,6 +140,7 @@ def comp_lvl_begginer():
                 
         else:
             holder = ([int(x) for x in random_comp().split(",")])
+            time.sleep(1)
             implement_move(holder,"O")
             X= True
             
@@ -173,85 +174,94 @@ def unbeatable_ai():
 def get_unbeatable_ai_move(human, i):
     # uznajac ze komputer zawsze gra jako O 
     if i == 1: # i == tura
-        if human == [0,0] or human == [2,2] or human == [0,2] or human == [2,0]:
-            return [1,1]
-        elif human == [1,1]:
+        if human == [1,1]:
             return [2,2]
-        elif human == [0,1] or human == [1,0] or human == [1,2] or human == [2,1]:
+        else:
             return [1,1]
     if i == 2: # i == tura
-        if tablica[1][1] == 'O': #trzeba dopisac wiecej if-ow czy jakies petle porobic
-            if tablica[0][0] == 'X':
-                if human == [0,1]:
+        if checkBestMove_ai() == None:
+            if tablica[1][1] == 'O':
+                if tablica[0][1]==tablica[1][2] !='.':
+                    return [0,2]
+                elif tablica[1][2]==tablica[2][1] !='.':
+                    return [2,2]
+                elif tablica[2][1]==tablica[1][0] !='.':
+                    return [2,0]
+                elif tablica[1][0]==tablica[0][1] !='.':
+                    return [0,0]
+                elif tablica[0][1]==tablica[2][1] !='.':
+                    return [2,2]
+                elif tablica[1][0]==tablica[1][2] !='.':
+                    return [2,2]
+                elif human == [0,2] or human == [1,2] or human == [2,2] and tablica[0][1] == '.':
+                    return [0,1]
+                elif human == [2,0] or human == [2,1] or human == [0,0] and tablica[1][0] == '.':
+                    return [1,0]
+                    
+            elif tablica[2][2] == 'O':
+                if human == [1,2]:
+                    return [1,0]
+                elif human == [0,2]:
+                    return [2,0]
+                elif human == [0,1]:
+                    return [2,1]
+                elif human == [0,0] or human == [2,0]:
                     return [0,2]
                 elif human == [1,0]:
-                    return [2,0]
-                elif human == [0,2] or human == [1,2] or human == [2,2]:
+                    return [1,2]
+                elif human == [2,1]:
                     return [0,1]
-                elif human == [2,0] or human == [2,1]:
-                    return [1,0]
-            else:
-                return checkBestMove_ai()
-                
-        if tablica[2][2] == 'O':
-            if human == [1,2]:
-                return [1,0]
-            elif human == [0,2]:
-                return [2,0]
-            elif human == [0,1]:
-                return [2,1]
-            elif human == [0,0] or human == [2,0]:
-                return [0,2]
-            elif human == [1,0]:
-                return [1,2]
-            elif human == [2,1]:
-                return [0,1]
         else:
             return checkBestMove_ai()
     
-    if i == 3 or i == 4 or i == 5: #nie wiem jak bardzo potrzebne
+    if i == 3 or i == 4 or i == 5:
+        if checkBestMove_ai() == None:
+            if tablica[0][1]=='.': 
+                return [0,1]
+            elif tablica[1][0]=='.':
+                return [1,0]
+            elif tablica[2][1]=='.':
+                return [2,1]
+            elif tablica[1][2]=='.':
+                return [1,2]
         return checkBestMove_ai()
             
 def checkBestMove_ai():
     #Checks for win/block condition and returns win or block move
-    try:
+    if tablica[0][0]==tablica[1][1] and tablica[2][2] == '.':
+        return [2,2]
+    elif tablica[1][1]==tablica[2][2] and tablica[0][0] == '.':
+        return [0,0]
+    elif tablica[0][2]==tablica[1][1] and tablica[2][0] == '.':
+        return [2,0]
+    elif tablica[1][1]==tablica[2][0] and tablica[0][2] == '.':
+        return [0,2]
+    elif tablica[0][0]==tablica[2][2] and tablica[1][1] == '.': #naprawione z np. X: A3,C1 i O: B2
+        return [1,1]
+    elif tablica[2][0]==tablica[0][2] and tablica[1][1] == '.':
+        return [1,1]
+    else:
         for row in range(0,3):
             if tablica[row][0] == '.': #Checks rows FIRST column for win or block
-                if tablica[row][1] == tablica[row][2]:
+                if tablica[row][1] == tablica[row][2] != '.':
                     return [row,0]
             if tablica[row][1] == '.': #Checks rows SECOND column for win or block
-                if tablica[row][0] == tablica[row][2]:
+                if tablica[row][0] == tablica[row][2] != '.':
                     return [row,1]
             if tablica[row][2] == '.': #Checks rows THIRD column for win or block
-                if tablica[row][0] == tablica[row][1]:
+                if tablica[row][0] == tablica[row][1] != '.':
                     return [row,2]
-            
-
         for col in range(0,3):
             if tablica[0][col] == '.': #Checks columns FIRST row for win or block
-                if tablica[1][col] == tablica[2][col]:
+                if tablica[1][col] == tablica[2][col] != '.':
                     return [0,col]
             if tablica[1][col] == '.':
-                if tablica[0][col] == tablica[2][col]:
+                if tablica[0][col] == tablica[2][col] != '.':
                     return [1,col]
             if tablica[2][col] == '.': #Checks columns THIRD row for win or block
-                if tablica[0][col] == tablica[1][col]:
+                if tablica[0][col] == tablica[1][col] != '.':
                     return [2,col]
-
-        #Checks for win diagonally
-        if tablica[0][0]==tablica[1][1] and tablica[2][2] == '.':
-            return [2,2]
-        if tablica[1][1]==tablica[2][2] and tablica[0][0] == '.':
-            return [0,0]
-        if tablica[0][2]==tablica[1][1] and tablica[2][0] == '.':
-            return [2,0]
-        if tablica[1][1]==tablica[2][0] and tablica[0][2] == '.':
-            return [0,2]
-        if tablica[0][0]==tablica[2][2] or tablica[2][0]==tablica[0][2] and tablica[1][1] == '.':
-            return [1,1]
-    except:
-        return [int(x) for x in random_comp().split(",")] #nie wiem jak to zrobic, aktualnie kiedy plansza jest juz nie do wygrania/blokowania wyskakuje blad. Probowalem losowy znak zeby dawalo zeby sie zapelnilo ale jest jakis bug :(
-
+                
 def main_menu():
     print("1) Zagraj z człowiekiem")
     print("2) Zagraj z komputerem (lvl. begginer)")
